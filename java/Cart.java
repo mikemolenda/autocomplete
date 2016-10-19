2db4249fb54cb85fb157aa55f2bb90acb9e8e4b5
Mike Molenda <molenda.mike@gmail.com>
2016-10-19 10:46:55 -0500

    Removes name from header

--- java/Cart.java
+++ java/Cart.java
@@ -1,16 +1,7 @@
 /* 
  * Cart
  * 
- * Methods to access and modify the array of the user's ordered items.
- * 
- * Northwestern University
- * CIS 419 Web Application Development, Winter 2016
- * Week 10 Assignment #5
- *
- * March 13, 2016
- *
- * Mike Molenda
- * michaelmolenda2014@u.northwestern.edu 
+ * Methods to access and modify the array of the user's ordered items. 
  */
 
 import java.util.*;

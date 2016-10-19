f791f6255498b0967956fb60b56e33a5d121dc93
Mike Molenda <molenda.mike@gmail.com>
2016-10-19 10:48:16 -0500

    Removes name from header

--- java/Fulfillment.java
+++ java/Fulfillment.java
@@ -2,15 +2,6 @@
  * Fulfillment
  * 
  * Holds completed order data. Not persistent.
- * 
- * Northwestern University
- * CIS 419 Web Application Development, Winter 2016
- * Week 10 Assignment #5
- *
- * March 13, 2016
- *
- * Mike Molenda
- * michaelmolenda2014@u.northwestern.edu 
  */
 
 import java.io.*;
